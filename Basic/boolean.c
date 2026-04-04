20ba0abd5a5b47743a4db6adbc7e2311c22a1752
Khondokar Almikat <khondoker.almikat@gmail.com>
2026-04-05 00:08:18 +0600

    Add files via upload

---
+++ Basic/boolean.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool isTrue = true;
+    bool isFalse = false;
+
+    printf("Value of isTrue: %d\n", isTrue);
+    printf("Value of isFalse: %d\n", isFalse);
+
+    return 0;
+}
